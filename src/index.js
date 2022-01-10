3ffaa9bdb6a1954fb6c8dd5143b5d1f2a917aaf1
AlexanderVLRS <96099152+AlexanderVLRS@users.noreply.github.com>
2022-01-10 23:43:24 +1000

    feat: add massiv

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
+const { valid } = require("semver");
+
+const strNums = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "ninteen",
+];
+const str10th = [
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+];
+
 module.exports = function toReadable (number) {
+    let result = [];
+
+    function verIntegerNumber(number){
+    if (Number.isInteger(number)){
+        return number;
+        }
+    return "number is not valid";
+    }
+
+
+    if (number > 99) {
+      const x = Math.floor(number / 100);
+      result.push(strNums[x-1] + ' hundred');
+    }
   
-}
+    if (number < 100 && number > 19) {
+      const x = Math.floor(number / 10);
+      result.push(str10th[x-1]);
+    }
+  
+    if (x < 19) {
+      result.push(strNums[number]);
+    }
+  
+    return result.join(' ');  
+} 
