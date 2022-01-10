f7d95c875791b77b5ac7a73cbe53cd5f71882b24
AlexanderVLRS <96099152+AlexanderVLRS@users.noreply.github.com>
2022-01-11 01:01:54 +1000

    fix: bug fixes

--- src/index.js
+++ src/index.js
@@ -1,54 +1,28 @@
-const { valid } = require("semver");
-
-const strNums = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-    "ten",
-    "eleven",
-    "twelve",
-    "thirteen",
-    "fourteen",
-    "fifteen",
-    "sixteen",
-    "seventeen",
-    "eighteen",
-    "ninteen",
-];
-const str10th = [
-    "twenty",
-    "thirty",
-    "forty",
-    "fifty",
-    "sixty",
-    "seventy",
-    "eighty",
-    "ninety",
+const strNum = [
+    'zero','one','two','three','four', 'five','six','seven','eight','nine', 'ten','eleven','twelve',
+    'thirteen', 'fourteen', 'fifteen','sixteen', 'seventeen','eighteen','nineteen',
+    'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'
 ];
 
 module.exports = function toReadable (number) {
     let result = [];
-
-    if (number > 99) {
-      const x = Math.floor(number / 100);
-      result.push(strNums[x-1] + ' hundred');
+    if (number >= 100) {
+        const x = Math.floor(number / 100);
+        result.push(strNum[x], 'hundred');
+        number -= x * 100;
+        if (number === 0) {
+            return result.join(' ');
+        }
     }
-  
-    if (number < 100 && number > 19) {
-      const x = Math.floor(number / 10);
-      result.push(str10th[x-1]);
+    if (number > 19) {
+        const x = Math.floor(number / 10);
+        result.push(strNum[x+18]);
+        number -= x * 10
+        if (number > 0) {
+            result.push(strNum[number]);
+        }
+    } else {
+        result.push(strNum[number]);
     }
-  
-    if (x < 19) {
-      result.push(strNums[number]);
-    }
-  
-    return result.join(' ');  
-} 
+    return result.join(' ');
+}
